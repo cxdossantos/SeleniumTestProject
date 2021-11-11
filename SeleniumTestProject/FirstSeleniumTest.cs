846adad04037568019ebbdd5599a84ce317ba7f6
Cleiton Xavier dos Santos <cxdosantos@gmail.com>
2021-11-10 21:54:35 -0300

    Utilizando o 'Using'

--- SeleniumTestProject/FirstSeleniumTest.cs
+++ SeleniumTestProject/FirstSeleniumTest.cs
@@ -5,29 +5,19 @@ using Xunit;
 
 namespace SeleniumTestProject
 {
-    public class FirstSeleniumTest : IDisposable
+    public class FirstSeleniumTest
     {
-
-        // Fechando Navegador com interface IDisposable
-        private IWebDriver _driver;
-
-        public FirstSeleniumTest()
-        {
-            _driver = new ChromeDriver();
-            _driver.Manage().Window.Maximize();
-        }
 
         [Fact]
         public void CorrectTitleDisplayed_When_NavigateToHomePage()
         {
-            _driver.Navigate().GoToUrl("https://lambdatest.github.io/sample-todo-app/");
+            // Ao invés de utilizar uma variavel privada, podemos implementar com o 'using', desta forma ira fechar o navegador automaticamente
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://lambdatest.github.io/sample-todo-app/");
 
-            Assert.Equal("Sample page - lambdatest.com", _driver.Title);
-        }
-
-        public void Dispose()
-        {
-            _driver.Quit();
+                Assert.Equal("Sample page - lambdatest.com", driver.Title);
+            }
         }
     }
 }
