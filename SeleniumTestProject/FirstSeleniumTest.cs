e62fe11a32542f0600efcbbb67d88827a3c47ba7
Cleiton Xavier dos Santos <cxdosantos@gmail.com>
2021-11-10 23:01:46 -0300

    Execução headless com configurações do Driver Manager

--- SeleniumTestProject/FirstSeleniumTest.cs
+++ SeleniumTestProject/FirstSeleniumTest.cs
@@ -2,6 +2,9 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using System;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager.Helpers;
 using Xunit;
 
 namespace SeleniumTestProject
@@ -12,8 +15,12 @@ namespace SeleniumTestProject
         [Fact]
         public void CorrectTitleDisplayed_When_NavigateToHomePage()
         {
+            new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--headless");
+
             // Na ultima versão do dotnet, não é necessário utilizar 'chaves'
-            using var driver = new FirefoxDriver();
+            using var driver = new ChromeDriver();
 
             driver.Navigate().GoToUrl("https://lambdatest.github.io/sample-todo-app/");
             Assert.Equal("Sample page - lambdatest.com", driver.Title);
