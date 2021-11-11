f901d9a79c0adeef66b6b78c561811abd284f27d
Cleiton Xavier dos Santos <cxdosantos@gmail.com>
2021-11-10 21:36:19 -0300

    Adicionar arquivos de projeto.

---
+++ SeleniumTestProject/FirstSeleniumTest.cs
@@ -0,0 +1,33 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using Xunit;
+
+namespace SeleniumTestProject
+{
+    public class FirstSeleniumTest : IDisposable
+    {
+
+
+        private IWebDriver _driver;
+
+        public FirstSeleniumTest()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+        }
+
+        [Fact]
+        public void CorrectTitleDisplayed_When_NavigateToHomePage()
+        {
+            _driver.Navigate().GoToUrl("https://lambdatest.github.io/sample-todo-app/");
+
+            Assert.Equal("Sample page - lambdatest.com", _driver.Title);
+        }
+
+        public void Dispose()
+        {
+            _driver.Quit();
+        }
+    }
+}
